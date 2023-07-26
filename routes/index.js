a2287a09e4927e6dae7dfa137b61b98473110dc5
Akila Sooriyabandara <109722820+AkilaSooriyabandara@users.noreply.github.com>
2023-07-26 10:33:56 +1000

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+<table width="100%" border="1">
+  <tr>
+    <td>Akila</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td rowspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
